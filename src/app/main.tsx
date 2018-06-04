9ccff87190c2bd9b1646d8fa64b5d0db4f2bf2fb
Joseph Weidinger <jsphweid@gmail.com>
2018-06-03 22:54:24 -0500

    both components working together but not sure how effectively

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,12 +2,15 @@ import * as React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import MidiVisualizer from 'react-midi-visualizer'
+import Metronome from 'react-conductor'
+import { Note, MIDI } from 'midiconvert'
 
 export interface MainProps {}
 
 export interface MainState {
 	startTime: number
 	reactResetKey: string
+	isRecording: boolean
 }
 
 const myQuery = gql`
@@ -30,7 +33,8 @@ export default class Main extends React.Component<MainProps, MainState> {
 		super(props)
 		this.state = {
 			startTime: null,
-			reactResetKey: 'firstKey'
+			reactResetKey: 'firstKey',
+			isRecording: false
 		}
 	}
 
@@ -39,22 +43,54 @@ export default class Main extends React.Component<MainProps, MainState> {
 		this.audioContext = new AudioContext()
 	}
 
-	private renderMidiVisualizer(data: any): JSX.Element {
-		const notes = JSON.parse(data.segment.midiJson).tracks[0].notes
-		console.log('notes', notes)
+	private renderMidiVisualizer(notes: Note[]): JSX.Element {
 		return notes ? (
-			<div>
-				<button onClick={() => this.setState({ startTime: this.audioContext.currentTime })}>START!</button>
-				<MidiVisualizer
-					key={`MidiVisualizer${this.state.reactResetKey}`}
-					audioContext={this.audioContext}
-					height={500}
-					width={800}
-					startTime={this.state.startTime}
-					notes={notes}
-				/>
-			</div>
+			<MidiVisualizer
+				key={`MidiVisualizer${this.state.reactResetKey}`}
+				audioContext={this.audioContext}
+				height={500}
+				width={800}
+				startTime={this.state.startTime}
+				notes={notes}
+			/>
 		) : null
+	}
+
+	private renderMetronome(bpm: number): JSX.Element {
+		return (
+			<Metronome
+				audioContext={this.audioContext}
+				width={200}
+				height={200}
+				bpm={bpm}
+				startTime={this.state.startTime}
+				options={{}}
+			/>
+		)
+	}
+
+	private handleStartRecording(): void {
+		this.setState({ isRecording: true, startTime: this.audioContext.currentTime })
+	}
+
+	private handleStopRecording(): void {
+		this.setState({ isRecording: false, startTime: null })
+	}
+
+	private renderStartStopButton(): JSX.Element {
+		const { isRecording } = this.state
+		const buttonText = isRecording ? 'STOP' : 'START!'
+		const clickHandler = isRecording ? this.handleStopRecording : this.handleStartRecording
+		return <button onClick={clickHandler.bind(this)}>{buttonText}</button>
+	}
+
+	private handleNewSegment(refetch: Function): void {
+		const randomHash: string = Math.random()
+			.toString(36)
+			.substring(7)
+		this.setState({ reactResetKey: randomHash })
+		this.handleStopRecording()
+		refetch()
 	}
 
 	render() {
@@ -64,22 +100,18 @@ export default class Main extends React.Component<MainProps, MainState> {
 					{({ loading, error, data, refetch }) => {
 						if (loading) return 'Loading...'
 						if (error) return `Error! ${error.message}`
+						const midiJson = JSON.parse(data.segment.midiJson) as MIDI
+						const bpm = midiJson.header.bpm
+						const notes = midiJson.tracks[0].notes
 
 						return (
 							<div>
-								<button
-									onClick={() => {
-										this.setState({
-											reactResetKey: Math.random()
-												.toString(36)
-												.substring(7)
-										})
-										refetch()
-									}}
-								>
-									New Segment
-								</button>
-								{this.renderMidiVisualizer(data)}
+								<div>{this.renderMidiVisualizer(notes)}</div>
+								<div>
+									{this.renderStartStopButton()}
+									<button onClick={this.handleNewSegment.bind(this, refetch)}>New Segment</button>
+									{this.renderMetronome(bpm)}
+								</div>
 							</div>
 						)
 					}}
