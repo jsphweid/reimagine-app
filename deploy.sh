18a5b5e0e334862987be1f42e061962524ef1341
Joseph Weidinger <jsphweid@gmail.com>
2018-06-26 18:45:23 -0500

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+bucket_name=$(<~/.aws/main_bucket_name)
+cloudfront_id=$(<~/.aws/main_cloudfront_id)
+
+npm run build
+aws s3 sync ./dist/ s3://${bucket_name}/reimagine --delete
+aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
