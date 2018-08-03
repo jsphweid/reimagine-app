017c02a097841b89b2bc87fbc74b4046d537ac23
Joseph Weidinger <jsphweid@gmail.com>
2018-08-02 22:44:23 -0500

    fix build process

--- src/connectors/redux/index.ts
+++ src/connectors/redux/index.ts
@@ -6,14 +6,14 @@ import reducer from './reducers'
 let store
 
 // when statically rendering... don't enable devtools... (which requires the window object)
-if (typeof window === 'undefined') {
-	store = createStore(reducer, applyMiddleware(thunk))
-} else {
-	const win: any = window
+const win: any = window
+if (typeof window !== 'undefined' && win.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
 	store = createStore(
 		reducer,
 		win.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(thunk))
 	)
+} else {
+	store = createStore(reducer, applyMiddleware(thunk))
 }
 
 export default store
