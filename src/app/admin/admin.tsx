818e27c9afca3320d8b6763f9d2c3ce80a24e4de
jsphweid <jsphweid@gmail.com>
2022-02-13 21:56:07 -0600

    more work in admin area to manage arrangements

--- src/app/admin/admin.tsx
+++ src/app/admin/admin.tsx
@@ -1,54 +1,27 @@
-import React from "react";
-
 import Section from "../small-components/section";
-import AudioDropzone from "./audio-dropzone";
+import { useGetAllPiecesQuery } from "../../generated";
+import Arrangements from "./arrangements";
 
 function Admin() {
-  const [rejectedFiles, setRejectedFiles] = React.useState<any[]>([]);
+  const { data, loading } = useGetAllPiecesQuery();
 
-  function onDrop(acceptedFiles: any[], rejected: any[]) {
-    setRejectedFiles(rejected);
-    // this.props.dispatch(postMidiFiles(acceptedFiles));
+  function renderPieceAndArrangements() {
+    if (data && data.getAllPieces) {
+      const name = data.getAllPieces[0]?.name;
+      const pieceId = data.getAllPieces[0]?.id;
+      return (
+        <>
+          <div>Using piece {name}</div>
+          <div>{pieceId ? <Arrangements pieceId={pieceId} /> : null}</div>
+        </>
+      );
+    }
+
+    return loading ? "Loading Piece" : null;
   }
 
-  function renderPossibleRejectedFilesError() {
-    return rejectedFiles && rejectedFiles.length ? (
-      <div>
-        There are {rejectedFiles.length} files in this batch that cannot be
-        uploaded because they aren't midi files.
-      </div>
-    ) : null;
-  }
-
-  function renderPostMidiInfo() {
-    // const { uploadingMidiFailure, uploadingMidi, idsOfLatestUpload } =
-    //   this.props;
-    // switch (true) {
-    //   case !!uploadingMidiFailure:
-    //     return <p>{uploadingMidiFailure}</p>;
-    //   case uploadingMidi:
-    //     return <p>Uploading</p>;
-    //   case !!idsOfLatestUpload && !!idsOfLatestUpload.length:
-    //     return <p>{JSON.stringify(idsOfLatestUpload)}</p>;
-    //   default:
-    //     return null;
-    // }
-  }
-
-  return (
-    <Section>
-      <AudioDropzone
-        onFiles={(files) =>
-          console.log(
-            "TODO -- do something with these files:",
-            files.map((f) => f.name)
-          )
-        }
-      />
-      {renderPostMidiInfo()}
-      {renderPossibleRejectedFilesError()}
-    </Section>
-  );
+  // TODO: add in error handling for wrong file types or multiple files.
+  return <Section>{renderPieceAndArrangements()}</Section>;
 }
 
 export default Admin;
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,12 @@ export function removeTypename<T extends { __typename?: any }>(
     __typename: undefined,
   };
 }
+
+// stolen from https://stackoverflow.com/questions/36280818/how-to-convert-file-to-base64-in-javascript
+export const toBase64 = (file: File): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
