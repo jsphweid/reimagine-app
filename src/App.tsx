603b56860244b5d199463cf489ffe735f755225f
Joseph Weidinger <jsphweid@gmail.com>
2022-03-04 18:47:52 -0600

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import ReactGa from "react-ga";
+
 import Main from "./app/main";
 import "./app.scss";
+
+ReactGa.initialize("G-7CDJBDRTTX");
 
 export const App: React.FC = () => {
   return <Main />;
