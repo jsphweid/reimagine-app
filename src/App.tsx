c5c488ce2634a5b7d899cf16f76bc8f36e70bbfa
Joseph Weidinger <jsphweid@gmail.com>
2022-03-04 18:53:56 -0600

    goof

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactGa from "react-ga";
 import Main from "./app/main";
 import "./app.scss";
 
-ReactGa.initialize("G-7CDJBDRTTX");
+ReactGa.initialize("UA-222099610-1");
 
 export const App: React.FC = () => {
   return <Main />;
